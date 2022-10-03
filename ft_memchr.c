2cfbb0728ec6157ffa5ca6ba8f17b2de4ff18f31
Rosa Ana Ruiz Victoria <roruiz-v@c3r8s2.42malaga.com>
2022-10-03 14:32:44 +0200

    con memchr todo ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,10 +6,33 @@
 /*   By: roruiz-v <roruiz-v@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 16:24:55 by roruiz-v          #+#    #+#             */
-/*   Updated: 2022/10/01 16:30:09 by roruiz-v         ###   ########.fr       */
+/*   Updated: 2022/10/03 14:25:20 by roruiz-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*src;
+	unsigned char	search;
+	size_t			i;
+
+	src = (unsigned char *) s;
+	search = (unsigned char) c;
+	i = 0;
+	if (n != 0)
+	{
+		while (*src != search && i < n - 1)
+		{
+			i++;
+			src++;
+		}
+		if (*src == search)
+			return (src);
+		else
+			return (NULL);
+	}
+	else
+		return (NULL);
+}
