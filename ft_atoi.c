9c55b8c42110a6427ee0b60c44f1d0455c2cfacf
Rosa Ana Ruiz Victoria <roruiz-v@c3r7s1.42malaga.com>
2022-10-06 09:15:04 +0200

    atoi OK +25 lines

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: roruiz-v <roruiz-v@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/04 19:00:53 by roruiz-v          #+#    #+#             */
-/*   Updated: 2022/10/05 12:14:56 by roruiz-v         ###   ########.fr       */
+/*   Created: 2022/10/05 16:41:54 by roruiz-v          #+#    #+#             */
+/*   Updated: 2022/10/06 08:12:04 by roruiz-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,63 @@
   Converts the initial portion of the string pointed (passing spaces until
   	finds a number, and then from there to last until finds a no-number
   	character, where it stops).
+	Omite los caracteres en blanco anteriores (como espacios, 
+		sangría de tabulación, etc.), no inicia la conversión hasta que encuentra 
+		un número o un signo, y luego encuentra un non-number O. 
+	La conversión termina cuando la cadena termina ('\0') 
+		y se devuelve el resultado.
   RETURN VALUE: the int representation of the string found.
+
+	j va a conservar la posición del primer número que encuentre;
+	i-- va a conservar la posición del último número que encuentre.
+	(tengo que ir recorriendo en orden inverso: unidades, decenas, centenas...)
 */
 #include "libft.h"
-
-int	ft_strchrtoint(char *str_num)
-{
-	int	k;
-	int	nbr;
-	int ascii;
-
-	k = 0;
-	nbr = 0;
-	while (str_num[k] != '\0')
-	{
-		if (str_num[k] == 48)
-			nbr = nbr + 0;
-		k++;
-	}
-	return (nbr);
-}
 
 int	ft_atoi(const char *str)
 {
 	size_t	i;
 	size_t	j;
-	char	*str_num;
+	int		num;
+	int		weight;
+	int		sign;
 
 	i = 0;
 	j = 0;
-	while (!ft_isnumber(str[i]) && str[i] != '\0')
-		i++;
-	if (ft_isnumber(str[i]))
+	num = 0;
+	sign = 0;
+	weight = 1;
+	while (!ft_isdigit(str[i]) && str[i] != '\0')
 	{
-		while (ft_isnumber(str[i]) && str[i] != '\0')
-		{	
-			str_num[j] = str[i];
+		if (ft_isdigit(str[i]) || (str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 			i++;
-			j++;
+		else if ((str[i] == '+' || str[i] == '-') && sign == 0)
+		{
+			if (str[i] == '+')
+				sign = +1;
+			else if (str[i] == '-')
+				sign = -1;
+		i++;
+			if (!ft_isdigit(str[i]))
+				return (0);
 		}
-		str_num[j] = '\0';
-// Ahora transformo los char en int uno a uno y los voy sumando en nbr:		
-// Llamo a la función que se va a encargar de transformar char en int:
-		return (ft_strchrtoint(str_num));
+		else
+			return (0);
 	}
-	return (NULL);
+	if (ft_isdigit(str[i]))
+	{
+		j = i;
+		while (ft_isdigit(str[i]) && str[i] != '\0')
+			i++;
+		while (i-- > j)
+		{
+			num = num + (str[i] - 48) * weight;
+			weight = weight * 10;
+		}
+		if (sign != 0)
+			return (num * sign);
+		else
+			return (num);
+	}
+	return (0);
 }
