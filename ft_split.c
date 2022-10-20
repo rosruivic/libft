a830cad4492f55b878ae02855419020b2a2d6422
Rosa Ana Ruiz Victoria <roruiz-v@c3r2s1.42malaga.com>
2022-10-20 18:10:30 +0200

    20-10-22 split con substr y bad behavior c at the end

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: roruiz-v <roruiz-v@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 13:54:36 by roruiz-v          #+#    #+#             */
-/*   Updated: 2022/10/19 18:27:06 by roruiz-v         ###   ########.fr       */
+/*   Updated: 2022/10/20 18:03:55 by roruiz-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	**ft_freedom(char **matrix, int word)
+static char	**ft_freedom(char **matrix, size_t word)
 {	
 	while (word >= 0)
 	{
@@ -23,111 +23,75 @@ static char	**ft_freedom(char **matrix, int word)
 	return (NULL);
 }
 
-/** ft_calcrows detecta separadores (cuenta cuántas palabras hay en strim)
-strim es la string inicial limpiada de caracteres separadores a inicio y fin */
-static size_t	ft_calcrows(char *strim, char c)
+static size_t	ft_calcrows(const char *s, char c)
 {
-	int		i;
+	size_t	i;
 	size_t	words;
 
 	i = 0;
 	words = 0;
-	while (strim[i] != '\0')
+	while (s[i] != '\0')
 	{
-		while (strim[i] != c && strim[i] != '\0')
-		{
+		while (s[i] == c && s[i] != '\0')
 			i++;
-		}
-		if (strim[i] == c)
-		{
-			if (strim[i + 1] != c)
-				words++;
+		if (s[i] != c && s[i] != '\0')
+			words++;
+		i++;
+		while (s[i] != c && s[i] != '\0')
 			i++;
-		}
-	}
-	return (words + 1);
+	}	
+	return (words);
 }
 
-//static char	**ft_putword(char **matrix, char *strim, size_t word, int wlen)
-//{
-//	int	j;
-//	int	w_ini;
-//
-//	j = 0;
-//	w_ini = 0;
-//}
-
-// ***********************************************************************
-//
-// 	cada vez que encuentro un separador (con STRCHR):
-//		- DIMENSIONO y MALLOKEO LAS POSICIONES de ESA fila 
-//		- EXTRAIGO con SUBSTR, MUDÁNDOLA a ESA fila de la MATRIX,
-//		- Y DESTRUYO CONTENIDO DE PALABRA MUDADA con las tijeras STRTRIM, 
-//		- dejando la string preparada para la siguiente vuelta hasta su fin
-//	(al final, esta idea no ha sido desarrollada, ha salido otra cosa)
-//	
 //	***********************************************************************
 //	word 	=> para saber en qué fila de la matriz estoy (= num palabra)	
-//	i 		=> para saber en qué posición de strim estoy (lo recorro entero)
-//	j 		=> para saber en qué posición de cada palabra de la matrix estoy
+//	i 		=> para saber en qué posición de s estoy (lo recorro entero)
 //	w_ini => para saber la primera posición de la palabra en la que estoy
-//	w_len 	=> para saber la longitud de la palabra con la que estoy
 //	***********************************************************************
-static char	**ft_matrix_posit(char **matrix, char *strim, char c)
+//	POR MIS OVARIOS, AQUÍ AHORA VOY A USAR EL SUBSTR, QUE PA ESTO ES
+//	***********************************************************************
+static char	**ft_matrix_posit(char **matrix, const char *s, char c, size_t words)
 {
 	size_t	word;
-	int		i;	
-	int		j;
-	int		w_ini;
+	size_t	i;	
+	size_t	w_ini;
 
 	word = 0;
 	i = 0;
-	while (word <= ft_calcrows(strim, c) && strim[i] != '\0')
-	{
-		j = 0;
-		while (strim[i] == c && strim[i] != '\0')
+	while (word <= words && s[i] != '\0')
+	{	
+		while (s[i] == c && s[i] != '\0')
+		{
 			i++;
-		w_ini = i;
-		while (strim[i] != c && strim[i] != '\0')
-			i++;
+		}
+		if (s[i] != c && s[i] != '\0')
+		{
+			w_ini = i;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+		}
 		matrix[word] = (char *)ft_calloc(((i - w_ini) + 1), sizeof(char));
 		if (!matrix)
 		{
 			ft_freedom(matrix, word);
 			return (NULL);
 		}
-		while (j < (i - w_ini))
-		{
-			matrix[word][j] = strim[w_ini + j];
-			j++;
-		}
-		matrix[word][j] = '\0';
+		matrix[word] = ft_substr(s, (unsigned int)w_ini, (i - w_ini));
 		word++;
-	}
+	}	
 	return (matrix);
 }
 
-// para poder usar strtrim, tengo que mandar c dentro de un array (set)
-// que primero mallokeo (y libero tras haber enviado el dato a strtrim)
 char	**ft_split(char const *s, char c)
 {
-	char		*strim;
-	char		*set;
 	char		**matrix;
 	size_t		words;
 
-	words = 0;
-	set = (char *)ft_calloc(2, sizeof(char));
-	if (!set)
-		return (NULL);
-	set[0] = c;
-	strim = ft_strtrim(s, set);
-	free (set);
-	set = NULL;
-	words = ft_calcrows(strim, c);
+	words = ft_calcrows(s, c);
 	matrix = (char **)ft_calloc(words + 1, sizeof(char *));
 	if (!matrix)
-		return (NULL);
-	matrix = ft_matrix_posit(matrix, strim, c);
+			return (NULL);
+	if (words > 0)
+		matrix = ft_matrix_posit(matrix, s, c, words);
 	return (matrix);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: roruiz-v <roruiz-v@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 13:59:26 by roruiz-v          #+#    #+#             */
-/*   Updated: 2022/10/19 13:19:59 by roruiz-v         ###   ########.fr       */
+/*   Updated: 2022/10/20 12:13:29 by roruiz-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,16 @@ static size_t	ft_getstart(char const *s1, char const *set)
 	return (i);
 }
 
+char	*ft_strvoid(void)
+{
+	char	*strvoid;
+
+	strvoid = (char *)ft_calloc(1, sizeof(char));
+	if (!strvoid)
+		return (NULL);
+	return (strvoid);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
@@ -37,12 +47,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	stop;
 
 	if (s1 == NULL || s1[0] == '\0')
-	{
-		newstr = (char *)ft_calloc(1, sizeof(char));
-		if (!newstr)
-			return (NULL);
-		return (newstr);
-	}
+		return (ft_strvoid());
 	if (!set)
 		return (ft_substr(s1, 0, ft_strlen(s1) + 1));
 	j = ft_strlen(s1) - 1;
@@ -55,6 +60,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		else
 			stop = '1';
 	}
-	newstr = ft_substr(s1, i, (j - i + 1));
+	newstr = ft_substr(s1, i, (j - i) + 1);
 	return (newstr);
 }
